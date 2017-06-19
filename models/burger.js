7b6fc870928e4ec38f5f6da75fc3b59935e9bdd3
haley <haleyhester@Haleys-MacBook-Air-7.local>
2017-06-18 23:09:14 -0500

    Node added
    node added. I also fixed and modified the Modals

---
+++ models/burger.js
@@ -0,0 +1,17 @@
+
+module.exports = function(sequelize, DataTypes) {
+
+ var Burger = sequelize.define('burger', {
+
+  burger_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    len: [1]
+  },
+  devoured: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+    }
+  });
+  return Burger;
+};
